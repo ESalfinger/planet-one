b392635403b730fe7ba53b97af9d61fa143e3562
Clemens <scholz.clemens@gmail.com>
2019-11-02 14:38:45 +0100

    More video Testing
    Does also not work

--- src/VideoTest.js
+++ src/VideoTest.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Backgound from './bg.jpg';
 import { Player } from 'video-react';
+import "../node_modules/video-react/dist/video-react.css";
+import { Video } from './TransparentBG.webm';
 
 export function VideoTest() {  
     return (
-        <div> 
-            <Player autoPlay fluid>
-                <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-            </Player>
-            <img className = 'background' src = { Backgound }></img>
+        <div>
+            <video className = 'video' controls autoPlay>
+                <source src = { Video }/>
+            </video>
+            <img className = 'background' src = { Backgound } alt='background'></img>
         </div>
     );
 }
