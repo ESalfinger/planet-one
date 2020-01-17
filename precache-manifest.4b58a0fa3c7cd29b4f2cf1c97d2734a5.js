3fc7b4d5f7dd819fb9e4ddd5c32a70cd13e216cf
Elias <s1810238068@students.fh-hagenberg.at>
2020-01-17 11:34:35 +0100

    Updates

---
+++ precache-manifest.4b58a0fa3c7cd29b4f2cf1c97d2734a5.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bda0d438d0547c54da038e7038f489",
+    "url": "/project-klima/index.html"
+  },
+  {
+    "revision": "55cacb12803f5387e65c",
+    "url": "/project-klima/static/css/main.b30101c2.chunk.css"
+  },
+  {
+    "revision": "0077e8b3fefa3e9d8137",
+    "url": "/project-klima/static/js/2.d7f918b3.chunk.js"
+  },
+  {
+    "revision": "55cacb12803f5387e65c",
+    "url": "/project-klima/static/js/main.c66a0037.chunk.js"
+  },
+  {
+    "revision": "25428cf814c1dc878c5a",
+    "url": "/project-klima/static/js/runtime-main.a33e6abb.js"
+  },
+  {
+    "revision": "ccf65f4e88a3482562fe2df004b6445f",
+    "url": "/project-klima/static/media/BalooBhaijaan-Regular.ccf65f4e.ttf"
+  },
+  {
+    "revision": "ae0faee2a997cc696e997c6e97f1f1c4",
+    "url": "/project-klima/static/media/Car.ae0faee2.webm"
+  },
+  {
+    "revision": "0b444b8da7456c87c8b19ea37037137c",
+    "url": "/project-klima/static/media/Car_drop.0b444b8d.webm"
+  },
+  {
+    "revision": "6313ca59f484dadde186270bf1f59c74",
+    "url": "/project-klima/static/media/FloatingIsland_Cloudy.6313ca59.webm"
+  },
+  {
+    "revision": "454caf7cbdd3f97afe3538b8aa4e0be5",
+    "url": "/project-klima/static/media/FloatingIsland_Snow.454caf7c.webm"
+  },
+  {
+    "revision": "0b827d43818fe0baa0280c0c14bee5ac",
+    "url": "/project-klima/static/media/FloatingIsland_Sun.0b827d43.webm"
+  },
+  {
+    "revision": "7944399c244951debd2c3fd2f3925478",
+    "url": "/project-klima/static/media/Mountain.7944399c.webm"
+  },
+  {
+    "revision": "0ff57533e2e9ed043a78b705a2f2e50f",
+    "url": "/project-klima/static/media/Plane.0ff57533.webm"
+  },
+  {
+    "revision": "365bd983634ea2c3f448c8f779c39e4c",
+    "url": "/project-klima/static/media/Planet.365bd983.webm"
+  },
+  {
+    "revision": "e2ea525e36bb9c7982748ca062bb8b85",
+    "url": "/project-klima/static/media/Rocket.e2ea525e.webm"
+  },
+  {
+    "revision": "8b255927aa699cd06f17230168ddc8ea",
+    "url": "/project-klima/static/media/arrow_down.8b255927.svg"
+  },
+  {
+    "revision": "046797d416149c76daa6c1052f727987",
+    "url": "/project-klima/static/media/back.046797d4.svg"
+  },
+  {
+    "revision": "3f56418e933577e7392404234d3e4216",
+    "url": "/project-klima/static/media/forward.3f56418e.svg"
+  },
+  {
+    "revision": "b75fd44593598883f9199eb05ef9290c",
+    "url": "/project-klima/static/media/nav.b75fd445.svg"
+  },
+  {
+    "revision": "b9d8a9d6cacea7f1844f0a83b50912a0",
+    "url": "/project-klima/static/media/navOpen.b9d8a9d6.svg"
+  },
+  {
+    "revision": "819734bd6fd26ce71aac61bd0106bf35",
+    "url": "/project-klima/static/media/search.819734bd.svg"
+  },
+  {
+    "revision": "81d0fc07e223701c1e1c84db106bfbe3",
+    "url": "/project-klima/static/media/spaceBackground.81d0fc07.jpg"
+  }
+]);
