2097dccf1124acd7c0a281b86da2378d1f933c3f
Clemens <scholz.clemens@gmail.com>
2020-01-20 22:48:59 +0100

    Video and Texts can now have multiple parameter

--- src/Section.js
+++ src/Section.js
@@ -19,16 +19,19 @@ import i18n from 'i18next';
 import k from "./i18n/keys";
 
 export function Section(props) {
-  function getHTML() {
-    return i18n.t(k[(props.className + 'text').toUpperCase()]);
+  function getHTML(text) {
+    return i18n.t(k[(text + 'text').toUpperCase()]);
   }
   return (
     <div className = {"section " + props.className} id = {props.className} style = {props.className === 'space' ? {backgroundImage: 'url(' + spaceBG + ')', backgroundSize: 'cover'} : {}}>
-      <div className='text'>
-        {props.className !== "weather" && props.className !== "animals" && props.className !== "hero" && <h1>{i18n.t(k[(props.className + 'title').toUpperCase()])}</h1>}
-        <p dangerouslySetInnerHTML={{__html: getHTML()}}></p>
-      </div>
-      {props.src && <Video src={props.src} />}
+      {props.texts && props.texts.map((text) => 
+        <div className='text'>
+         <p dangerouslySetInnerHTML={{__html: getHTML(text)}}></p>
+        </div>
+      )}
+      { props.src && props.src.map((video) => 
+        <Video id = {video} key = {video} src = {video} />
+      )}
       {props.className === 'hero' && <Quote />}
       {props.button && <Button action={props.action} target={props.target} symbol={props.symbol} className={props.button} buttonText={props.buttonText} />}
       {(props.className === "animals") && <Cards type={props.className} />}
